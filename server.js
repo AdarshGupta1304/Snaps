55ffac4c90a1d3d7cd71091869bdea24f367ac97
Adarsh Gupta <87adarsh.gupta@gmail.com>
2021-01-24 19:36:47 +0530

    Commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const DB_NAME = process.env.GLOBAL_DB ; 
 
 mongoose.connect(DB_NAME, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then(con => console.log('DB connection successfully established !')).catch(err => console.log("MongoDB error: ",err));
+.then(con => console.log('DB connection successfully established !')).catch(err => console.log("MongoDB error :: ",err));
 
 
 const port =  process.env.PORT ||3000;
